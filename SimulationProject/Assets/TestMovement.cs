f60c8483678bbdc6faa65ff7a959ad10adb6345b
CelestialMagic <116318917+CelestialMagic@users.noreply.github.com>
2023-01-12 11:38:54 -0800

    Added TestMovement Script
    -Restored Grass and FollowCam

---
+++ SimulationProject/Assets/TestMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TestMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveForce;
+
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    private float rotation;
+
+    private Vector3 m_ToApplyMove;//A Vector3 representing the player movement force
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Determines direction of key press
+        if (Input.GetKey(KeyCode.W))
+        {
+            m_ToApplyMove += new Vector3(0, 0, moveForce * Time.deltaTime);
+
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            m_ToApplyMove += new Vector3(0, 0, -moveForce * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            m_ToApplyMove += new Vector3(moveForce * Time.deltaTime, 0, 0);
+
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            m_ToApplyMove += new Vector3(-moveForce * Time.deltaTime, 0, 0);
+
+        }
+
+
+
+
+
+    }
+
+    protected void FixedUpdate()
+    {
+        //Applies force to rigidbody to allow player to jump
+        //Resets to zero after. 
+        rb.AddForce(m_ToApplyMove);
+        m_ToApplyMove = Vector3.zero;
+
+
+
+    }
+}
