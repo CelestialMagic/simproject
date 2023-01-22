2e661c04dc638f7d5aa35ff0de05ac2071d0a89c
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2023-01-22 11:28:07 -0800

    Implemented PlayerSpawner
    -ScriptableObject Players can be spawned into the game!
    -This code can be expanded for local multiplayer as well

--- SimulationProject/Assets/Scripts/SOscripts/Player.cs
+++ SimulationProject/Assets/Scripts/SOscripts/Player.cs
@@ -7,8 +7,14 @@ public class Player : ScriptableObject
 {
     [SerializeField]
     public PlayerMovement prefab;
-
     [SerializeField]
-    public Color color; 
+    public Color color;
 
+
+    public PlayerMovement Spawn()
+    {
+        PlayerMovement instance = GameObject.Instantiate(prefab);
+        instance.Initialize(color);
+        return instance;
+    }
 }
