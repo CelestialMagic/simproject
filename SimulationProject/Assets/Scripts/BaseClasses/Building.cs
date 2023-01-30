62d073a2700f73ba2b1026b3fc8478e99a5e60f6
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2023-01-30 11:14:24 -0800

    Added Interfaces Folder

--- SimulationProject/Assets/Scripts/BaseClasses/Building.cs
+++ SimulationProject/Assets/Scripts/BaseClasses/Building.cs
@@ -7,12 +7,7 @@ public class Building : MonoBehaviour
 {
     private void Start()
     {
-        Health healthScript = this.GetComponent<Health>();
-        if (healthScript) healthScript.onDeath += Destroy_OnDeath;
-        //subscribe to destroy self when death event is emitted
+      
     }
-    private void Destroy_OnDeath(object sender, EventArgs e)
-    {
-        Destroy(gameObject);
-    }
+  
 }
