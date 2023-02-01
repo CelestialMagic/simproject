07be94b54abb04381ad8461a4c7a5a8ad4d79fa5
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2023-02-01 14:18:57 -0800

    Added variable SFX

--- SimulationProject/Assets/Scripts/BaseClasses/Animal.cs
+++ SimulationProject/Assets/Scripts/BaseClasses/Animal.cs
@@ -5,24 +5,48 @@ using UnityEngine;
 public abstract class Animal : ObjectFactory
 {
     [SerializeField]
-    protected AudioClip noise;
+    protected AudioClip noise;//An AudioClip representing an animal noise
 
     [SerializeField]
-    protected float volume;
+    protected float volume;//The volume of the animal noise played
 
     [SerializeField]
-    protected AudioSource audioSource;
+    protected AudioSource audioSource;//The audio source for the animal
 
     [SerializeField]
-    protected float audioTimer;
+    protected float audioTimer;//The countdown variable to play audio
 
     [SerializeField]
-    protected float resetTimer;
+    protected float resetTimer;//A float to reset the timer to a given time
 
+    [SerializeField]
+    protected float waitPeriod;//A float representing seconds to vary when
+                               //sound is played
+
+    //Plays an audioclip
     protected void PlaySound(AudioClip sound, float volume)
     {
         audioSource.PlayOneShot(sound, volume);
     }
 
+    //Code for creating an AnimalObject
+    public override void CreateObject()
+    {
+        Instantiate(gameObject);
+    }
 
+    //Update() is used primarily to play animal sound effects
+    //This may be updated for AI
+    private void Update()
+    {
+        if (audioTimer - Time.deltaTime <= 0)
+        {
+            PlaySound(noise, volume);
+            audioTimer = Random.Range(resetTimer - waitPeriod, resetTimer + waitPeriod);
+        }
+        else
+        {
+            audioTimer -= Time.deltaTime;
+        }
+    }
 }
