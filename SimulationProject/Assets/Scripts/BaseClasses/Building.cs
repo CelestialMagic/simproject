873016f80159e47b69860f1885e52e2084ffc742
CelestialMagic <116318917+CelestialMagic@users.noreply.github.com>
2023-02-02 11:03:51 -0800

    Added a barrier in front of zoo

--- SimulationProject/Assets/Scripts/BaseClasses/Building.cs
+++ SimulationProject/Assets/Scripts/BaseClasses/Building.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 
 public abstract class Building : ObjectFactory
 {
-    
-    
+    [SerializeField] GameObject m_prefab;
+    [SerializeField] int m_cost;
+    public GameObject prefab
+    {
+        get { return m_prefab; }
+        set { prefab = m_prefab; }
+    }
+
+
+    public int cost
+    {
+        get { return m_cost; }
+        set { cost = m_cost; }
+    }
+
+
+
 
     public override void CreateObject()
     {
