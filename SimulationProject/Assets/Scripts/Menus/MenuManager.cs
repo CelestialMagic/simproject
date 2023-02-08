3adb366d8bc82f30df7b25deb8c6305133d22400
CelestialMagic <116318917+CelestialMagic@users.noreply.github.com>
2023-02-08 14:38:25 -0800

    Updated Gift Shop

--- SimulationProject/Assets/Scripts/Menus/MenuManager.cs
+++ SimulationProject/Assets/Scripts/Menus/MenuManager.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System;
 
+//MenuManager is a work-in-progress script to be developed in Playtest 2
 public class MenuManager : MonoBehaviour
 {
-    public static MenuManager instance;
+    public static MenuManager instance;//The MenuManager in scene
     [SerializeField]
-    private GameObject[] menuTypes;
+    private GameObject[] menuTypes;//A list of available menu types
     [SerializeField]
-    private GameObject playerToListen;
+    private GameObject playerToListen;//A player to follow
 
-    public static Action MenuControl;
+    public static Action MenuControl;//An action to be eventually used
 
     [SerializeField]
-    private InputManager pInput; 
+    private InputManager pInput; //InputManager for player
 
+    //Singleton code
     private void Awake()
     {
         if (instance != null)
