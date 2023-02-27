c31787f10dba6e812fdcc2fdec98f49e2717661e
goozmatic <115593900+goozmatic@users.noreply.github.com>
2023-02-27 00:19:37 -0800

    Setting up navmeshes for other animals
    -still need to fix pathfinding within pens

--- SimulationProject/Assets/Scripts/AnimalFamily/Lion.cs
+++ SimulationProject/Assets/Scripts/AnimalFamily/Lion.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Lion : Animal, ISpawnableObject
 {
-    //The overridden Update() method is intentionally left blank.
+    //The overridden Update() method removes the MakeNoise() method.
     //The inherited version will automatically play sound. 
     protected override void Update()
     {
-        
+        LocateNextSpot();
     }
 
     //lion roars if the player gets too close
